d3143b48b652ccb6d149dc64140ea0313ef74976
igotyabingo <jenifer7933@snu.ac.kr>
2025-10-01 20:56:22 +0900

    WIP: apply changes after review / update PR feedback

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -6,7 +6,21 @@ class User(models.Model):  # 모델 명 User로 변경
     # id 필드: 기본으로 생성됨
     email = models.EmailField(max_length=255, unique=True)  # EmailField로 변경, unique 속성 설정
     name = models.CharField(max_length=30)
-    refresh_token = models.CharField(max_length=512, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} <{self.email}>"
+
+
+class GoogleAccount(models.Model):  # 구글 API 접근을 위한 모델
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="google_accounts", unique=True
+    )  # 실제 필드명은 자동으로 user_id가 됨
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    expires_at = models.DateTimeField()  # 토큰 만료 시간
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"GoogleAccount for {self.user.email}"
--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -2,14 +2,10 @@ from django.urls import path
 
 from .views import (
     GoogleCallbackView,
-    GoogleStartView,
     LogoutView,
-    google_refresh,
 )
 
 urlpatterns = [
-    path("google/start/", GoogleStartView.as_view(), name="google_start"),
     path("google/callback/", GoogleCallbackView.as_view(), name="google_callback"),
-    path("google/refresh/", google_refresh, name="google_refresh"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
