989446dbaf89762388266b31d22ae840fe42d745
kwish <kwish@snu.ac.kr>
2025-10-13 17:05:25 +0900

    fix: remove some duplicated urls

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
 
 if settings.ENVIRONMENT in [ServerEnvironmentType.LOCAL, ServerEnvironmentType.DEV]:
     urlpatterns += [
-        path("swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-        path("schema/", SpectacularAPIView.as_view(), name="schema"),
         path("schema/", SpectacularAPIView.as_view(), name="schema"),
         path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
         path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
