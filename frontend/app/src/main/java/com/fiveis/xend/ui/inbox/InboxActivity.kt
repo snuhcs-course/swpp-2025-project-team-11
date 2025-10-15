55674123eca0114bd8ffd261cef5769ebfe82e6a
Jaewan Park <pona4321@snu.ac.kr>
2025-10-15 20:52:09 +0900

    feat: modify transition animation between inbox and contactbook

--- frontend/app/src/main/java/com/fiveis/xend/ui/inbox/InboxActivity.kt
+++ frontend/app/src/main/java/com/fiveis/xend/ui/inbox/InboxActivity.kt
@@ -3,17 +3,30 @@ package com.fiveis.xend.ui.inbox
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.fiveis.xend.R
 import com.fiveis.xend.ui.compose.MailComposeActivity
 import com.fiveis.xend.ui.contactbook.ContactBookActivity
 
 class InboxActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    finish()
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                }
+            }
+        )
+
         setContent {
             MaterialTheme {
                 val viewModel: InboxViewModel = viewModel()
@@ -29,6 +42,7 @@ class InboxActivity : ComponentActivity() {
                     onBottomNavChange = {
                         if (it == "contacts") {
                             startActivity(Intent(this, ContactBookActivity::class.java))
+                            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
                         }
                     }
                 )
