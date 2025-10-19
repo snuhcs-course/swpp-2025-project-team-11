2d686c26f4e2eb88ecd310c78f2122810f60bd2d
junho_maeng <104117184+s3mng@users.noreply.github.com>
2025-10-19 18:16:30 +0900

    feat: search/ mail save

--- frontend/app/src/main/java/com/fiveis/xend/ui/compose/MailComposeActivity.kt
+++ frontend/app/src/main/java/com/fiveis/xend/ui/compose/MailComposeActivity.kt
@@ -937,7 +937,7 @@ class MailComposeActivity : ComponentActivity() {
                 // Hoisted states
                 var subject by rememberSaveable { mutableStateOf("") }
                 val richTextState = rememberRichTextState()
-                // var body by rememberSaveable { mutableStateOf("") }
+
                 var contacts by remember { mutableStateOf(emptyList<Contact>()) }
                 var newContact by remember { mutableStateOf(TextFieldValue("")) }
 
@@ -1025,11 +1025,7 @@ private fun EmailComposePreview() {
             onSubjectChange = {},
             richTextState = richTextState,
             contacts = listOf(Contact("Id here", "홍길동", "test@example.com", groupId = "GroupId here")),
-            /**
-             * body = "초안 본문...",
-             * onBodyChange = {},
-             * contacts = listOf(Contact(0, 0, "홍길동", "test@example.com")),
-            */
+
             onContactsChange = {},
             newContact = TextFieldValue(""),
             onNewContactChange = {},
