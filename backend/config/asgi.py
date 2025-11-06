db9cb4b9c8d2a109897780400eb070694e61ccbe
kwish <kwish@snu.ac.kr>
2025-11-06 11:28:38 +0900

    fix: import modules error

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,13 +1,13 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from apps.ai.routing import websocket_urlpatterns
 from config.utils import set_environment
 
 set_environment()
 django_asgi_app = get_asgi_application()
 # Django 기본 ASGI application
 
+from apps.ai.routing import websocket_urlpatterns  # noqa: E402
 from config.jwt_middleware import JWTAuthMiddleware  # noqa: E402
 
 # ProtocolTypeRouter: HTTP / WebSocket 분기
