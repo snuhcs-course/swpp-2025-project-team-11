73afe12e5bb216e1cde6ef25525b3840f03de708
Jaewan-Park-Student <pona4321@snu.ac.kr>
2025-11-12 18:14:17 +0900

    fix: import Modifier in ReplyDirectComposeActivity

--- frontend/app/src/main/java/com/fiveis/xend/ui/view/ReplyDirectComposeActivity.kt
+++ frontend/app/src/main/java/com/fiveis/xend/ui/view/ReplyDirectComposeActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
